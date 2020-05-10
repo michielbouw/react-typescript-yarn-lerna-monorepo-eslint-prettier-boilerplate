369a3b95d75e62e7bd42f5071d866c18072e6371
Michiel Bouw <mpbouw94@gmail.com>
2020-05-10 12:12:10 +0300

    update to latest react-scripts

--- packages/www/src/App.test.tsx
+++ packages/www/src/App.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/get more info on create react app/i);
+  render(<App />);
+  const linkElement = screen.getByText(/get more info on create react app/i);
   expect(linkElement).toBeInTheDocument();
 });
